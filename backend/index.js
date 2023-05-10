a9cd3a8246777d3c0ba8660ec430e1d6ad65741f
Sahil V N <iss225@iss2.local>
2023-05-10 15:20:16 +0530

    chat app

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+});
+io.on('connection', (socket) => {
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log(`user With ID ${socket.id} joined in room : ${data}`);
+  });
+  socket.on('send_message', (data) => {
+    socket.to(data.room).emit('recieve_message', data);
+  });
+  socket.on('disconnect', () => {
+    console.log('user Disconnected', socket.id);
+  });
+});
+server.listen(3001, () => {
+  console.log('listening');
+});
